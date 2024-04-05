b163da85611ffa6bbdb314a4ddd2031059814d5b
AliyaSalmanova1 <136350408+AliyaSalmanova1@users.noreply.github.com>
2024-04-04 21:16:18 -0400

    first page is working besides 'next step' button

--- src/App.js
+++ src/App.js
@@ -4,13 +4,27 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import SelectionBox from "./components/Selections.js"
 import PersonalInfo from "./components/Info.js"
 import MainSection from "./components/Main.js"
+import { useState } from "react";
 
 function App() {
+
+  const [personalInfo, setPersonalInfo] = useState({
+    name: '',
+    email: '',
+    phone: ''
+  })
+
+  const [plan, setPlan] = useState({
+    plan: '',
+    monthYear: ''
+  })
+
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SelectionBox />}>
-          <Route index element={<PersonalInfo />}/>
+          <Route index element={<PersonalInfo personalInfo={personalInfo} setPersonalInfo={setPersonalInfo}/>}/>
           <Route path="plan" element={<PersonalInfo />}/>
           <Route path="add-ons" element={<PersonalInfo />}/>
           <Route path="summary" element={<PersonalInfo />}/>
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,11 +1,49 @@
 
-function PersonalInfo(){
+function PersonalInfo({personalInfo, setPersonalInfo}){
+
+
+
+  
+
 
     return (
-        <>personal info</>
+        <>
+            <form className="infoForm">
+                <label>Name<br/>
+                    <input 
+                    value={personalInfo.name}
+                    onChange={(e) => {
+                        setPersonalInfo({...personalInfo, name: e.target.value})}} 
+                    type="text"
+                    />
+                </label> 
+                <label>Email Address<br/>
+                    <input 
+                    value={personalInfo.email}
+                    onChange={(e) => {
+                        setPersonalInfo({...personalInfo, email: e.target.value})}} 
+                    type="email"
+                    />
+                </label> 
+                <label>Phone Number<br/>
+                <input 
+                    value={personalInfo.phone}
+                    onChange={(e) => {
+                        setPersonalInfo({...personalInfo, phone: e.target.value})}} 
+                    type="number"
+                    />
+                </label> 
+                
+
+                
+                
+                
+            </form>
+            <button>Next Step</button>
+            
+        </>
+        
     )
-
-
 
 }
 
